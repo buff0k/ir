2e3c4cef47b5c68dc031e44558d8624a1aca2aec
Eben van Deventer <buff0k@gmail.com>
2025-01-09 07:42:03 +0200

    modified:   ir/hooks.py 	modified:   ir/industrial_relations/doctype/appeal_against_outcome/appeal_against_outcome.json 	new file:   ir/industrial_relations/doctype/appeal_grounds_selector/__init__.py 	new file:   ir/industrial_relations/doctype/appeal_grounds_selector/appeal_grounds_selector.json 	new file:   ir/industrial_relations/doctype/appeal_grounds_selector/appeal_grounds_selector.py 	new file:   ir/industrial_relations/doctype/grounds_for_appeal/__init__.py 	new file:   ir/industrial_relations/doctype/grounds_for_appeal/grounds_for_appeal.js 	new file:   ir/industrial_relations/doctype/grounds_for_appeal/grounds_for_appeal.json 	new file:   ir/industrial_relations/doctype/grounds_for_appeal/grounds_for_appeal.py 	new file:   ir/industrial_relations/doctype/grounds_for_appeal/test_grounds_for_appeal.py 	modified:   ir/industrial_relations/print_format/contract_of_employment/contract_of_employment.json 	modified:   ir/industrial_relations/workspace/industrial_relations/industrial_relations.json Fixed Print Format for Contract of Employment Initiated work on Appeal Format

--- ir/hooks.py
+++ ir/hooks.py
@@ -24,7 +24,8 @@ fixtures = [
 		{"dt": "Report", "filters": [["name", "in", ["Disciplinary Offence Report"]]]},
 		{"dt": "Contract Section", "filters": [["sec_head", "in", ["Working Hours Placeholder", "Remuneration Placeholder"]]]},
 		{"dt": "Employee Rights", "filters": [["name", "in", ["Demotion", "Disciplinary Hearing", "Dismissal", "Incapacity", "Pay Deduction", "Pay Reduction", "Suspension", "Warning Form"]]]},
-		{"dt": "Module Profile", "filters": [["name", "in", ["Industrial Relations"]]]}
+		{"dt": "Module Profile", "filters": [["name", "in", ["Industrial Relations"]]]},
+		{"dt": "Grounds for Appeal", "filters": [["name", "in", ["Procedural", "Substantive", "New Evidence"]]]}
         ]
 scheduler_events = {"weekly": ["ir.industrial_relations.controllers.send_weekly_hr_report.send_weekly_hr_report"]}
 # Includes in <head>
