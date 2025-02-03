cb506bf8f98902c79a15d91817d6270ab6256d5e
Eben van Deventer <buff0k@gmail.com>
2025-02-03 13:20:46 +0200

    modified:   ir/industrial_relations/doctype/external_dispute_resolution/external_dispute_resolution.js 	modified:   ir/industrial_relations/doctype/external_dispute_resolution/external_dispute_resolution.json 	modified:   ir/industrial_relations/doctype/external_dispute_resolution/external_dispute_resolution.py 	modified:   ir/industrial_relations/doctype/external_dispute_resolution_applicants/external_dispute_resolution_applicants.json 	modified:   ir/industrial_relations/workspace/industrial_relations/industrial_relations.json Push External Dispute Resolution for Prod Testing

--- ir/industrial_relations/doctype/external_dispute_resolution/external_dispute_resolution.js
+++ ir/industrial_relations/doctype/external_dispute_resolution/external_dispute_resolution.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2025, BuFf0k and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("External Dispute Resolution", {
-// 	refresh(frm) {
+frappe.ui.form.on("External Dispute Resolution", {
+    employee: function (frm) {
+        let employees = (frm.doc.employee || []).map(e => e.employee);  // Extract employee IDs
+        let existing_applicants = (frm.doc.applicant_history || []).map(a => a.applicant);
 
-// 	},
-// });
+        // Remove rows for employees no longer selected
+        frm.doc.applicant_history = frm.doc.applicant_history.filter(a => employees.includes(a.applicant));
+
+        // Add missing employees
+        employees.forEach(emp => {
+            if (!existing_applicants.includes(emp)) {
+                let row = frm.add_child("applicant_history");
+                row.applicant = emp;
+            }
+        });
+
+        frm.refresh_field("applicant_history");
+    }
+});
