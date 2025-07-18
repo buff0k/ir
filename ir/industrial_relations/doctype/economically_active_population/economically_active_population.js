6a3c0b58b1bcff4d2aca9703191d1addb3229134
Isambane Group Domain <isambane@isex1.isambane.co.za>
2025-07-18 12:34:56 +0200

    modified:   ir/industrial_relations/doctype/economically_active_population/economically_active_population.js 	modified:   ir/industrial_relations/doctype/economically_active_population/economically_active_population.json 	modified:   ir/industrial_relations/doctype/economically_active_population/economically_active_population.py 	modified:   ir/industrial_relations/doctype/site_organogram/site_organogram.json Implemented basic data fields for EAP as per EEA

--- ir/industrial_relations/doctype/economically_active_population/economically_active_population.js
+++ ir/industrial_relations/doctype/economically_active_population/economically_active_population.js
@@ -1,8 +1,70 @@
 // Copyright (c) 2025, BuFf0k and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Economically Active Population", {
-// 	refresh(frm) {
+frappe.ui.form.on('Economically Active Population', {
+    african_male: function(frm) {
+        if (frm.doc.african_male !== undefined) {
+            update_total(frm);
+        }
+    },
+    african_female: function(frm) {
+        if (frm.doc.african_female !== undefined) {
+            update_total(frm);
+        }
+    },
+    coloured_male: function(frm) {
+        if (frm.doc.coloured_male !== undefined) {
+            update_total(frm);
+        }
+    },
+    coloured_female: function(frm) {
+        if (frm.doc.coloured_female !== undefined) {
+            update_total(frm);
+        }
+    },
+    indian_male: function(frm) {
+        if (frm.doc.indian_male !== undefined) {
+            update_total(frm);
+        }
+    },
+    indian_female: function(frm) {
+        if (frm.doc.indian_female !== undefined) {
+            update_total(frm);
+        }
+    },
+    white_male: function(frm) {
+        if (frm.doc.white_male !== undefined) {
+            update_total(frm);
+        }
+    },
+    white_female: function(frm) {
+        if (frm.doc.white_female !== undefined) {
+            update_total(frm);
+        }
+    },
 
-// 	},
-// });
+    onload_post_render: function(frm) {
+        update_total(frm);
+    },
+
+    validate: function(frm) {
+        if (flt(frm.doc.total) > 100) {
+            frappe.throw(__('Total cannot exceed 100%'));
+        }
+    }
+});
+
+function update_total(frm) {
+    let total = 0;
+
+    total += flt(frm.doc.african_male);
+    total += flt(frm.doc.african_female);
+    total += flt(frm.doc.coloured_male);
+    total += flt(frm.doc.coloured_female);
+    total += flt(frm.doc.indian_male);
+    total += flt(frm.doc.indian_female);
+    total += flt(frm.doc.white_male);
+    total += flt(frm.doc.white_female);
+
+    frm.set_value('total', total.toFixed(2));
+}
--- ir/industrial_relations/doctype/economically_active_population/economically_active_population.py
+++ ir/industrial_relations/doctype/economically_active_population/economically_active_population.py
@@ -1,9 +1,21 @@
 # Copyright (c) 2025, BuFf0k and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class EconomicallyActivePopulation(Document):
 	pass
+
+def validate(self):
+    total = (
+        flt(self.african_male) + flt(self.african_female) +
+        flt(self.coloured_male) + flt(self.coloured_female) +
+        flt(self.indian_male) + flt(self.indian_female) +
+        flt(self.white_male) + flt(self.white_female)
+    )
+    self.total = round(total, 2)
+    
+    if total > 100:
+        frappe.throw(_("Total cannot exceed 100%"))
