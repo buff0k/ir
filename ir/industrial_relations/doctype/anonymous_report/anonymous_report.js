c00e5c6f36b223f96cec34efb7b581fc684a2a67
buff0k <buff0k@gmail.com>
2026-04-08 08:55:32 +0200

    modified:   ir/hooks.py 	modified:   ir/industrial_relations/doctype/anonymous_report/anonymous_report.js 	modified:   ir/industrial_relations/doctype/anonymous_report/anonymous_report.json 	modified:   ir/industrial_relations/doctype/anonymous_report/anonymous_report.py 	modified:   pyproject.toml Version Bump and completed initial investigation notes for Anonymous Reports

--- ir/industrial_relations/doctype/anonymous_report/anonymous_report.js
+++ ir/industrial_relations/doctype/anonymous_report/anonymous_report.js
@@ -1,8 +1,29 @@
 // Copyright (c) 2026, BuFf0k and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Anonymous Report", {
-// 	refresh(frm) {
+frappe.ui.form.on("Anonymous Report", {
+	investigator: function (frm) {
+		if (!frm.doc.investigator) {
+			frm.set_value("investigator_name", "");
+			frm.set_value("investigator_designation", "");
+			return;
+		}
 
-// 	},
-// });
+		frappe.db.get_value("Employee", frm.doc.investigator, [
+			"employee_name",
+			"designation"
+		]).then((r) => {
+			if (r.message) {
+				frm.set_value("investigator_name", r.message.employee_name || "");
+				frm.set_value("investigator_designation", r.message.designation || "");
+			}
+		});
+	},
+
+	validate: function (frm) {
+		// Extra client-side guard
+		if (frm.doc.docstatus === 0 && frm.doc.__unsaved && frm.doc._action === "submit" && !frm.doc.outcome) {
+			frappe.throw(__("Outcome is required before submitting this document."));
+		}
+	}
+});
--- ir/industrial_relations/doctype/anonymous_report/anonymous_report.py
+++ ir/industrial_relations/doctype/anonymous_report/anonymous_report.py
@@ -1,9 +1,29 @@
 # Copyright (c) 2026, BuFf0k and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class AnonymousReport(Document):
-	pass
+	def validate(self):
+		if self.investigator:
+			employee_name, designation = frappe.db.get_value(
+				"Employee",
+				self.investigator,
+				["employee_name", "designation"]
+			) or (None, None)
+
+			self.investigator_name = employee_name or ""
+			self.investigator_designation = designation or ""
+
+		else:
+			self.investigator_name = ""
+			self.investigator_designation = ""
+
+	def before_submit(self):
+		if not self.investigator:
+			frappe.throw("An Investigator must be allocated before submitting this document.")
+
+		if not self.outcome:
+			frappe.throw("Outcome is required before submitting this document.")
