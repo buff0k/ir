32e796f6b300faad4d6225be203d250a0dbfade3
buff0k <buff0k@gmail.com>
2026-04-13 08:13:14 +0200

    deleted:    ir/fixtures/designated_group.json 	deleted:    ir/industrial_relations/doctype/designated_group/__init__.py 	deleted:    ir/industrial_relations/doctype/designated_group/designated_group.js 	deleted:    ir/industrial_relations/doctype/designated_group/designated_group.json 	deleted:    ir/industrial_relations/doctype/designated_group/designated_group.py 	deleted:    ir/industrial_relations/doctype/designated_group/test_designated_group.py Remove EE DocTypes that are already in ZA Local

--- ir/industrial_relations/doctype/designated_group/designated_group.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, BuFf0k and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Designated Group", {
-// 	refresh(frm) {
-
-// 	},
-// });
