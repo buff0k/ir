66a53ce0152ad28885103cbd11b45621f350d76d
buff0k <buff0k@gmail.com>
2026-05-07 19:01:50 +0200

    deleted:    ir/industrial_relations/doctype/employment_equity_sectoral_target_table/__init__.py 	deleted:    ir/industrial_relations/doctype/employment_equity_sectoral_target_table/employment_equity_sectoral_target_table.json 	deleted:    ir/industrial_relations/doctype/employment_equity_sectoral_target_table/employment_equity_sectoral_target_table.py 	deleted:    ir/industrial_relations/doctype/employment_equity_sectoral_targets/__init__.py 	deleted:    ir/industrial_relations/doctype/employment_equity_sectoral_targets/employment_equity_sectoral_targets.js 	deleted:    ir/industrial_relations/doctype/employment_equity_sectoral_targets/employment_equity_sectoral_targets.json 	deleted:    ir/industrial_relations/doctype/employment_equity_sectoral_targets/employment_equity_sectoral_targets.py 	deleted:    ir/industrial_relations/doctype/employment_equity_sectoral_targets/test_employment_equity_sectoral_targets.py 	deleted:    ir/industrial_relations/doctype/employment_equity_sectors/__init__.py 	deleted:    ir/industrial_relations/doctype/employment_equity_sectors/employment_equity_sectors.js 	deleted:    ir/industrial_relations/doctype/employment_equity_sectors/employment_equity_sectors.json 	deleted:    ir/industrial_relations/doctype/employment_equity_sectors/employment_equity_sectors.py 	deleted:    ir/industrial_relations/doctype/employment_equity_sectors/test_employment_equity_sectors.py 	deleted:    ir/industrial_relations/doctype/employment_equity_table/__init__.py 	deleted:    ir/industrial_relations/doctype/employment_equity_table/employment_equity_table.json 	deleted:    ir/industrial_relations/doctype/employment_equity_table/employment_equity_table.py 	deleted:    ir/industrial_relations/doctype/employment_equity_target/__init__.py 	deleted:    ir/industrial_relations/doctype/employment_equity_target/employment_equity_target.js 	deleted:    ir/industrial_relations/doctype/employment_equity_target/employment_equity_target.json 	deleted:    ir/industrial_relations/doctype/employment_equity_target/employment_equity_target.py 	deleted:    ir/industrial_relations/doctype/employment_equity_target/test_employment_equity_target.py 	deleted:    ir/industrial_relations/doctype/occupational_level/__init__.py 	deleted:    ir/industrial_relations/doctype/occupational_level/occupational_level.js 	deleted:    ir/industrial_relations/doctype/occupational_level/occupational_level.json 	deleted:    ir/industrial_relations/doctype/occupational_level/occupational_level.py 	deleted:    ir/industrial_relations/doctype/occupational_level/test_occupational_level.py 	deleted:    ir/industrial_relations/report/employment_equity_workforce_profile/__init__.py 	deleted:    ir/industrial_relations/report/employment_equity_workforce_profile/employment_equity_workforce_profile.js 	deleted:    ir/industrial_relations/report/employment_equity_workforce_profile/employment_equity_workforce_profile.json 	deleted:    ir/industrial_relations/report/employment_equity_workforce_profile/employment_equity_workforce_profile.py 	modified:   ir/industrial_relations/workspace/industrial_relations/industrial_relations.json 	modified:   ir/industrial_relations/workspace/ir_setup/ir_setup.json Removed legacy EE work from ir

--- ir/industrial_relations/doctype/employment_equity_sectoral_targets/employment_equity_sectoral_targets.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, BuFf0k and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Employment Equity Sectoral Targets", {
-// 	refresh(frm) {
-
-// 	},
-// });
