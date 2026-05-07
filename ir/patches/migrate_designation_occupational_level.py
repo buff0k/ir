b1b1adca9d6e77317b29f890329183193199996a
buff0k <buff0k@gmail.com>
2026-05-07 18:33:40 +0200

    modified:   ir/patches.txt 	new file:   ir/patches/migrate_designation_occupational_level.py Continue work in migrating to za_local EE

---
+++ ir/patches/migrate_designation_occupational_level.py
@@ -0,0 +1,75 @@
+# Copyright (c) 2026, BuFf0k and contributors
+# For license information, please see license.txt
+
+import frappe
+
+
+OLD_TO_NEW_OCCUPATIONAL_LEVEL = {
+    "Top management": "Top Management",
+    "Senior management": "Senior Management",
+    "Professionally qualified": "Professionally Qualified",
+    "Skilled technical": "Skilled Technical",
+    "Semi-skilled": "Semi-Skilled",
+    "Unskilled": "Unskilled",
+}
+
+
+def execute():
+    doctype = "Designation"
+
+    required_fields = [
+        "custom_occupational_level",
+        "ir_occupational_level",
+    ]
+
+    missing_fields = [
+        fieldname
+        for fieldname in required_fields
+        if not frappe.db.has_column(doctype, fieldname)
+    ]
+
+    if missing_fields:
+        frappe.throw(
+            "Cannot migrate Designation occupational level. Missing columns on Designation: {0}".format(
+                ", ".join(missing_fields)
+            )
+        )
+
+    migrate_occupational_level()
+
+
+def migrate_occupational_level():
+    for old_value, new_value in OLD_TO_NEW_OCCUPATIONAL_LEVEL.items():
+        frappe.db.sql(
+            """
+            UPDATE `tabDesignation`
+            SET ir_occupational_level = %(new_value)s
+            WHERE custom_occupational_level = %(old_value)s
+            """,
+            {
+                "old_value": old_value,
+                "new_value": new_value,
+            },
+        )
+
+    unmapped_values = frappe.db.sql(
+        """
+        SELECT DISTINCT custom_occupational_level
+        FROM `tabDesignation`
+        WHERE custom_occupational_level IS NOT NULL
+          AND custom_occupational_level != ''
+          AND custom_occupational_level NOT IN %(mapped_values)s
+        """,
+        {
+            "mapped_values": tuple(OLD_TO_NEW_OCCUPATIONAL_LEVEL.keys()),
+        },
+        as_dict=True,
+    )
+
+    if unmapped_values:
+        frappe.log_error(
+            title="Unmapped Designation occupational levels",
+            message="\n".join(
+                row.custom_occupational_level for row in unmapped_values
+            ),
+        )
