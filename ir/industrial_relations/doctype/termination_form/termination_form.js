e8779e48fcdf93b94714b2c70c4cefd863748f9f
buff0k <buff0k@gmail.com>
2026-09-14 07:47:18 +0200

    Fix Weekly Digest notifications not honouring restriction on External Dispute Resolution mails as well as Naming schema for Termination Forms which prevented an Employee from terminating twice, added guard against duplicating real termination forms

--- ir/industrial_relations/doctype/termination_form/termination_form.js
+++ ir/industrial_relations/doctype/termination_form/termination_form.js
@@ -81,7 +81,6 @@ frappe.ui.form.on("Termination Form", {
                     frm.set_value('requested_for_names', employee.employee_name || '');
                     frm.set_value('id_number', employee.za_id_number || '');
                     frm.set_value('company', employee.company || '');
-                    frm.set_value('doc_name', frm.doc.requested_for);
                 })
                 .catch(err => {
                     frappe.msgprint(__('Failed to fetch employee details'));
